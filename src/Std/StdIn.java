3cbbc4271eb433ec5356124acf7a8ec6a56ed91a
deviltt <tt416926063@qq.com>
2019-05-23 14:36:20 +0800

    从标准输入中读取byte, short, int, long, float, double, boolean等

--- src/Std/StdIn.java
+++ src/Std/StdIn.java
@@ -1,5 +1,6 @@
 package Std;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -34,7 +35,9 @@ public final class StdIn {
     }
 
     /**
-     * @return
+     * 一次读取一个字符
+     *
+     * @return 返回是否有字符
      */
     public static boolean hasNextChar() {
         scanner.useDelimiter(EMPTY_PATTERN);
@@ -51,7 +54,7 @@ public final class StdIn {
     public static String readLine() {
         String line;
         try {
-            line = scanner.nextLine();
+            line = scanner.nextLine();  //一次读取一行
         } catch (NoSuchElementException e) {
             line = null;
         }
@@ -78,6 +81,11 @@ public final class StdIn {
         }
     }
 
+    /**
+     * 一次读取所有
+     *
+     * @return 一次性读取所有内容
+     */
     public static String readAll() {
         //如果没有下一行则返回空字符串""
         if (!scanner.hasNextLine()) {
@@ -86,5 +94,118 @@ public final class StdIn {
         String result = scanner.useDelimiter(EVERYTHING_PATTERN).next();
         scanner.useDelimiter(WHITESPACE_PATTERN);
         return result;
+    }
+
+    /**
+     * scanner.next();一次只能读到一个单词，开始会忽略到前面的空白字符，直到
+     * 第一次遇到有效字符为止
+     * scanner.nextLine();读取结束符前面的所有字符
+     *
+     * @return 读到的一个字符串(不含空格)
+     */
+    public static String readString() {
+        try {
+            return scanner.next();
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read a 'String' value from standard input, "
+                    + "but no more tokens are available");
+        }
+    }
+
+    /**
+     * 从标准输入中读取一个整数，如果输入的是字符或者其他符号则报错
+     *
+     * @return 返回读取的整数
+     */
+    public static int readInt() {
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            String token = scanner.next();
+            throw new InputMismatchException("attempts to read an 'int' value from standard input, "
+                    + "but the next token is\"" + token + "\"");
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read an 'int' value from standard input, "
+                    + "but no more tokens are available");
+        }
+    }
+
+    public static double readFloat() {
+        try {
+            return scanner.nextFloat();
+        } catch (InputMismatchException e) {
+            String token = scanner.next();
+            throw new InputMismatchException("attempts to read an 'float' value from standard input, "
+                    + "but the next token is\"" + token + "\"");
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read an 'float' value from standard input, "
+                    + "but no more tokens are available");
+        }
+    }
+
+    public static double readLong() {
+        try {
+            return scanner.nextLong();
+        } catch (InputMismatchException e) {
+            String token = scanner.next();
+            throw new InputMismatchException("attempts to read an 'long' value from standard input, "
+                    + "but the next token is\"" + token + "\"");
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read an 'long' value from standard input, "
+                    + "but no more tokens are available");
+        }
+    }
+
+    public static double readShort() {
+        try {
+            return scanner.nextShort();
+        } catch (InputMismatchException e) {
+            String token = scanner.next();
+            throw new InputMismatchException("attempts to read an 'short' value from standard input, "
+                    + "but the next token is\"" + token + "\"");
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read an 'short' value from standard input, "
+                    + "but no more tokens are available");
+        }
+    }
+
+    public static double readByte() {
+        try {
+            return scanner.nextByte();
+        } catch (InputMismatchException e) {
+            String token = scanner.next();
+            throw new InputMismatchException("attempts to read an 'byte' value from standard input, "
+                    + "but the next token is\"" + token + "\"");
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("attempts to read an 'byte' value from standard input, "
+                    + "but no more tokens are available");
+        }
+    }
+
+    public static boolean readBoolean() {
+        try {
+            String token = readString();
+//            if ("true".equalsIgnoreCase(token)) return true;
+//            if ("false".equalsIgnoreCase(token)) return false;
+//            if ("1".equals(token)) return true;
+//            if ("0".equals(token)) return false;
+            switch (token){
+                case "true":
+                    return true;
+                case "false":
+                    return false;
+                case "1":
+                    return true;
+                case "0":
+                    return false;
+                default:
+                    throw new InputMismatchException("attempts to read an 'boolean' value from standard input, "
+                            + "but the next token is\"" + token + "\"");
+            }
+
+        }catch (NoSuchElementException e){
+            throw new NoSuchElementException("attempts to read an 'byte' value from standard input, "
+                    + "but no more tokens are available");
+        }
     }
 }
