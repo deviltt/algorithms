e261682b3141af9cf4e99f8629f0b7c394443a1e
deviltt <tt416926063@qq.com>
2019-07-25 22:22:48 +0800

    标准差和方差

--- src/Std/StdStats.java
+++ src/Std/StdStats.java
@@ -85,6 +85,124 @@ public final class StdStats {
         return sum / a.length;
     }
 
+    public static double mean(double[] a, int lo, int hi) {
+        int length = hi - lo;
+        if (length == 0) {
+            return Double.NaN;
+        }
+
+        double sum = sum(a, lo, hi);
+        return sum / length;
+    }
+
+    public static double mean(int[] a) {
+        if (a.length == 0) {
+            return Double.NaN;
+        }
+        int sum = sum(a);
+        return 1.0 * sum / a.length;
+    }
+
+    /**
+     * 方差计算公式
+     *
+     * @param a
+     * @return
+     */
+    public static double var(double[] a) {
+        if (a.length == 0) {
+            return Double.NaN;
+        }
+        double avg = mean(a);
+        double sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+        }
+        return sum / (a.length - 1);
+    }
+
+    public static double var(double[] a, int lo, int hi) {
+        int length = hi - lo;
+        if (length == 0) {
+            return Double.NaN;
+        }
+        double avg = mean(a, lo, hi);
+        double sum = 0;
+        for (int i = lo; i < hi; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+        }
+        return sum / (length - 1);
+    }
+
+    public static double var(int[] a) {
+        if (a.length == 0) return Double.NaN;
+        double avg = mean(a);
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+        }
+        return sum / (a.length - 1);
+    }
+
+
+    public static double varp(double[] a) {
+        if (a.length == 0) return Double.NaN;
+        double avg = mean(a);
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+        }
+        return sum / a.length;
+    }
+
+    public static double varp(double[] a, int lo, int hi) {
+        int length = hi - lo;
+        if (length == 0) return Double.NaN;
+
+        double avg = mean(a, lo, hi);
+        double sum = 0.0;
+        for (int i = lo; i < hi; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+        }
+        return sum / length;
+    }
+
+    public static double stddev(double[] a) {
+        return Math.sqrt(var(a));
+    }
+
+    public static double stddev(int[] a) {
+        return Math.sqrt(var(a));
+    }
+
+    public static double stddev(double[] a, int lo, int hi) {
+        return Math.sqrt(var(a, lo, hi));
+    }
+
+    public static double stddevp(double[] a) {
+        return Math.sqrt(varp(a));
+    }
+
+    public static double stddevp(double[] a, int lo, int hi) {
+        return Math.sqrt(varp(a, lo, hi));
+    }
+
+    private static int sum(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+
+    private static double sum(double[] a, int lo, int hi) {
+        double sum = 0;
+        for (int i = lo; i < hi; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+
     private static double sum(double[] a) {
         double sum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -92,4 +210,6 @@ public final class StdStats {
         }
         return sum;
     }
+
+
 }
